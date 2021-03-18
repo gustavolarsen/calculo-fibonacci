5b78460d368cae0b5741bef35d481a9f0be7d10e
Gustavo Larsen <guslarsen@gmail.com>
2021-03-18 14:20:16 -0300

    Update CalculaFibonacci.js

--- CalculaFibonacci.js
+++ CalculaFibonacci.js
@@ -1,4 +1,4 @@
-/* Sequencia de
+/* Sequencia de Fibonacci
  * i = 0 = 0
  * i = 1 = 1
  * i = 2 = 1
@@ -10,45 +10,22 @@
 
 function calculaFibonacci(totalSequencia) {
   console.time('fibonacci');
+
   let sequencia = [];
   let penultimoResultado = 0;
   let ultimoResultado = 0;
 
-  for (let index = 0; index < totalSequencia; index++) {
-    ultimoResultado = aplicaFormula(index, ultimoResultado);
+  for (let index = 0; index <= totalSequencia; index++) {
+    ultimoResultado = fibonacci(index, ultimoResultado);
     sequencia.push(ultimoResultado);
   }
 
-  function aplicaFormula(indexAtual, ultimoResultado) {
-    if (indexAtual === 0 || indexAtual === 1) {
-      penultimoResultado = ultimoResultado;
-      return indexAtual;
-    } else {
-      let soma = penultimoResultado + ultimoResultado;
-      penultimoResultado = ultimoResultado;
-      return soma;
-    }
+  function fibonacci(index, ultimoResultado) {
+    let soma = penultimoResultado + ultimoResultado;
+    penultimoResultado = ultimoResultado;
+    return index === 0 || index === 1 ? index : soma;
   }
+
   console.timeEnd('fibonacci');
-  return sequencia.join(', ');
-}
-
-function calculaFibonacciRecursiva(totalSequencia) {
-  console.time('fibonacciRecursiva');
-  let sequencia = [];
-
-  for (let index = 0; index < totalSequencia; index++) {
-    let resultado = aplicaFormula(index);
-    sequencia.push(resultado);
-  }
-
-  function aplicaFormula(indexAtual) {
-    if (indexAtual === 0 || indexAtual === 1) {
-      return indexAtual;
-    } else {
-      return aplicaFormula(indexAtual - 1) + aplicaFormula(indexAtual - 2);
-    }
-  }
-  console.timeEnd('fibonacciRecursiva');
   return sequencia.join(', ');
 }
